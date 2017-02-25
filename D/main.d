abba2e298704d7b82822b1e44e4cdb15c86d22cf
katastic <ckatko@gmail.com>
2017-02-25 02:22:11 -0500

    Lots of dev

--- D/main.d
+++ D/main.d
@@ -1,18 +1,17 @@
+// SkiiFree'd - KATKO 2017
+//=============================================================================
+
 import std.stdio;
 import std.conv;
 import std.string;
 import std.format; //String.Format like C#?! Nope. Damn, like printf.
 
-
-//import allegro5;
 pragma(lib, "dallegro5");
 
 version(ALLEGRO_NO_PRAGMA_LIB)
 {
 
-}
-else
-{
+}else{
 	pragma(lib, "allegro");
 	pragma(lib, "allegro_primitives");
 	pragma(lib, "allegro_image");
@@ -28,11 +27,25 @@ import allegro5.allegro_ttf;
 import allegro5.allegro_color;
 
 
-
-
-
-//import globals; ???
+// CONSTANTS
 //=============================================================================
+immutable float JUMP_VELOCITY = 2.2F; //NYI
+
+//world dimensions
+immutable float maximum_x = 1000F; 	//NYI
+immutable float maximum_y = 20000F; //NYI
+immutable float maximum_z = 100F; 	//NYI
+
+//GLOBALS
+//=============================================================================
+ALLEGRO_CONFIG* 		cfg;
+ALLEGRO_DISPLAY* 		display;
+ALLEGRO_EVENT_QUEUE* 	queue;
+
+ALLEGRO_COLOR 			color1;
+ALLEGRO_COLOR 			color2;
+ALLEGRO_BITMAP* 		bmp;
+ALLEGRO_FONT* 			font;
 
 animation_t player_anim;
 animation_t monster_anim;
@@ -50,7 +63,7 @@ class animation_t
 		{
 		ALLEGRO_BITMAP *extra_frame = al_load_bitmap( toStringz(path));
 		frames ~= extra_frame;
-		//names = to!string(); //filler, what happens if not unique? Return first result?
+		//names = to!string(); 
 		names ~= "OOPS."; //filler, what happens if not unique? Return first result?
 		}
 		
@@ -79,8 +92,7 @@ class animation_t
 		
 	void empty(){}
 	}
-
-
+	
 void resources()	
 	{
 	player_anim	.load_extra_frame("./data/mysha.pcx");
@@ -89,11 +101,84 @@ void resources()
 	jump_anim	.load_extra_frame("./data/mysha.pcx");
 	}
 
-class object_t
+class object_t //could we use a drawable_object whereas object_t has re-usable functionality for a camera_t?
 	{
 	public:
-	float 		x, y;
-	float		x_vel, y_vel;
+	float 		x, y, z; //objects are centered at X/Y (not top-left) so we can easily follow other objects.
+	float		x_vel, y_vel, z_vel; //note Z is used for jumps.
+	float		width, height;
+
+	bool trips_you;
+	bool slows_you_down;
+	bool is_following_another_object; 
+	object_t object_to_follow; 
+
+	this()
+		{
+		trips_you = false;
+		slows_you_down = false;
+		is_following_another_object = false;
+		}
+		
+	void follow_object(object_t obj)
+		{
+		is_following_another_object = true;
+		object_to_follow = obj;
+		}
+	
+	bool is_colliding_with(object_t obj)
+		{
+		// I freakin' love D.
+		alias x2 = obj.x;
+		alias y2 = obj.y;
+		alias width2 = obj.width;
+		alias height2 = obj.height;
+		
+		/* from https://wiki.allegro.cc/index.php?title=Bounding_Box   GO ALLEGRO GO
+		*/
+			
+		if(	x  > x2 + width2  - 1 	|| 
+			y  > y2 + height2 - 1 	||
+			x2 > x  + width   - 1	||
+			y2 > y  + height  - 1)
+			{
+			return false;
+			}
+		return true;
+		}
+	
+	// INPUTS (do we support mouse input?)
+	// ------------------------------------------
+	void up(){}
+	void down(){}
+	void left(){}
+	void right(){}
+	void action(){} // ala space. for monster this would be EAT MWAHAHA. (or is that automatic?) LET HIM EAT OTHER PEOPLE AND STUFF TOO.
+	void click_at(float relative_x, float relatie_y){} //maybe? relative to object coordinate.
+	
+	// EVENTS
+	// ------------------------------------------
+	void on_tick()
+		{
+		if(is_following_another_object)
+			{
+			x = object_to_follow.x;
+			y = object_to_follow.y;
+			}
+		}
+
+	void on_collision(object_t other_obj)
+		{
+		}	
+	}
+	
+class camera_t : object_t 
+	{
+	// simply uses the follow object routines in object_t!
+	}
+
+class drawable_object_t : object_t
+	{
 	animation_t animation;
 
 	void draw(int frame)
@@ -102,82 +187,274 @@ class object_t
 		}
 	}
 	
-class tree_t : object_t
+class tree_t : drawable_object_t
 	{
-
+	this()
+		{
+		trips_you = true;
+		}
 	}
 
-class rock_t : object_t
+class rock_t : drawable_object_t
 	{
-
+	this()
+		{
+		trips_you = true;
+		}
 	}
 
-class large_rough_patch_t : object_t //slows you down.
+class large_rough_patch_t : drawable_object_t //slows you down.
 	{
-
+	this()
+		{
+		slows_you_down = true;
+		}
 	}
 
-class small_rough_patch_t : object_t //slows you down.
+class small_rough_patch_t : drawable_object_t //slows you down.
 	{
-
-	}
-	
-class sign_t : object_t
-	{
-	}
-
-
-class lift_stand_t : object_t //the building
-	{
-	}
-
-class lift_chair_t : object_t
-	{
-	}
-
-
-
-class tree_stump : object_t 
-	{
-
-	}
-
-
-
-class jump_t : object_t
-	{
-
-	}
-
-class monster_t : object_t
-	{
-
-	}
-
-class skiier_t : object_t
-	{
-
+	this()
+		{
+		slows_you_down = true;
+		}
 	}
 	
-class player_t : skiier_t
+class sign_t : drawable_object_t
+	{
+	this()
+		{
+		trips_you = true;
+		}
+	}
+
+class lift_stand_t : drawable_object_t //the building
+	{
+	this()
+		{
+		trips_you = true;
+		}
+	}
+
+class lift_chair_t : drawable_object_t
+	{
+	this()
+		{
+		}
+	}
+
+class tree_stump : drawable_object_t 
+	{
+	this()
+		{
+		trips_you = true; //fuck you. ;)
+		}
+	}
+
+class jump_t : drawable_object_t
+	{
+	// is a jump bool? OR, an event method!
+	// OMG, we could use this method to send ANY OBJECT FLYING, MWAHAHA.
+	override void on_collision(object_t other_obj) // I FREAKING LOVE EXPLICIT OVERRIDES.
+		{
+		alias o = other_obj; //this is kind of cool, we can CLEARLY label the variable, and then use a tiny/ABBREVIATION version.
+		//or alias player =
+		//or alias p =	
+		o.z_vel += JUMP_VELOCITY;
+		}
+	}
+
+class monster_t : drawable_object_t
+	{
+	override void on_tick()
+		{
+		//run torward assholes
+		// need a find_player method. (what about multiple players?)
+		// Do we also need an A* algorithm (or something more basic) for navigating around objects when stuck?
+		// How do we get the monster to zig-zag like in the game?
+		}
+		
+	override void on_collision(object_t other_obj) 
+		{
+		if(auto p = cast(player_t) other_obj)
+			{
+			// I'M GONNA EAT YOU, BUB.
+			}		
+		}
+	}
+
+class skier_t : drawable_object_t
+	{
+	override void on_tick()
+		{
+		x += x_vel;
+		y += y_vel;
+		z += z_vel;
+		
+		if(x < 0)
+			{
+			x_vel = 0;
+			x = 0;
+			}
+
+		if(y < 0)
+			{
+			y_vel = 0;
+			y = 0;
+			}
+
+		if(z < 0)
+			{
+			z_vel = 0;
+			z = 0;
+			}
+		}
+	}
+	
+class player_t : skier_t
 	{
 
 	}
-
 
 class world
 	{
-	object_t [] world;
+	object_t [] objects;
+	
+	void test()
+		{
+		player_t x;
+		objects ~= x; //polymorphism rules. 
+		}
+	
 	}
 
-void initialize()
+//https://www.allegro.cc/manual/5/keyboard.html
+class keybinding_t
 	{
+	ubyte allegro_key; // SEE allegro enum in above URL. (note the enum is UNNAMED. ugh.) Values should be below 255?
+	//function ptr to call(), how do we deal with ones that need arguments?
+	void function() keybinding_fptr;
+
+// 	AGAIN, do we support mouse clicks? Then no multiplayer support...
+//	void function(float, float) mouse_fptr;
+//	bool is_keyboard;
+	}
 	
+// WAIT, why not do this DIFFERNTLY, like this: 
+//	(instead of individual KEYS touching ANY OBJECT METHOD. Because what if we 
+// 		change objects? We have to FIND all keys associated with that object and 
+// 		change them.)
+alias ALLEGRO_KEY = ubyte;
+class keyset_t
+		{
+		object_t object_to_touch;
+		ALLEGRO_KEY [5] keys;
+		}
+		
+enum
+	{
+	UP_KEY = 0,
+	DOWN_KEY = 1,
+	LEFT_KEY = 2,
+	RIGHT_KEY = 3,
+	ACTION_KEY = 4
+	}
+	
+	
+class controller_t
+	{
+	keybinding_t [] bindings;
+	}
+
+bool initialize()
+	{
+	if (!al_init())
+		{
+		auto ver = al_get_allegro_version();
+		auto major = ver >> 24;
+		auto minor = (ver >> 16) & 255;
+		auto revision = (ver >> 8) & 255;
+		auto release = ver & 255;
+
+		writefln("The system Allegro version (%s.%s.%s.%s) does not match the version of this binding (%s.%s.%s.%s)",
+			major, minor, revision, release,
+			ALLEGRO_VERSION, ALLEGRO_SUB_VERSION, ALLEGRO_WIP_VERSION, ALLEGRO_RELEASE_NUMBER);
+
+		assert(0, "The system Allegro version does not match the version of this binding!"); //why didn't they do this as an assert to begin with?
+		}
+		
+	cfg = al_load_config_file("test.ini");
+	display = al_create_display(500, 500);
+	queue = al_create_event_queue();
+
+	if (!al_install_keyboard())      assert(0, "al_install_keyboard failed!");
+	if (!al_install_mouse())         assert(0, "al_install_mouse failed!");
+	if (!al_init_image_addon())      assert(0, "al_init_image_addon failed!");
+	if (!al_init_font_addon())       assert(0, "al_init_font_addon failed!");
+	if (!al_init_ttf_addon())        assert(0, "al_init_ttf_addon failed!");
+	if (!al_init_primitives_addon()) assert(0, "al_init_primitives_addon failed!");
+
+	al_register_event_source(queue, al_get_display_event_source(display));
+	al_register_event_source(queue, al_get_keyboard_event_source());
+	al_register_event_source(queue, al_get_mouse_event_source());
+	
+	bmp = al_load_bitmap("./data/mysha.pcx");
+	font = al_load_font("./data/DejaVuSans.ttf", 18, 0);
+
+	with(ALLEGRO_BLEND_MODE)
+		{
+		al_set_blender(ALLEGRO_BLEND_OPERATIONS.ALLEGRO_ADD, ALLEGRO_ALPHA, ALLEGRO_INVERSE_ALPHA);
+		}
+
+	color1 = al_color_hsl(0, 0, 0);
+	color2 = al_map_rgba_f(0.5, 0.25, 0.125, 1);
+	writefln("%s, %s, %s, %s", color1.r, color1.g, color2.b, color2.a);
+	
+	return 0;
 	}
 
 void execute()
 	{
-		
+	bool exit = false;
+	while(!exit)
+		{
+		ALLEGRO_EVENT event;
+		while(al_get_next_event(queue, &event))
+		{
+			switch(event.type)
+			{
+				case ALLEGRO_EVENT_DISPLAY_CLOSE:
+				{
+					exit = true;
+					break;
+				}
+				case ALLEGRO_EVENT_KEY_DOWN:
+				{
+					switch(event.keyboard.keycode)
+					{
+						case ALLEGRO_KEY_ESCAPE:
+						{
+							exit = true;
+							break;
+						}
+						default:
+					}
+					break;
+				}
+				case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+				{
+					exit = true;
+					break;
+				}
+				default:
+			}
+		}
+
+		al_clear_to_color(ALLEGRO_COLOR(0.5, 0.25, 0.125, 1));
+		al_draw_bitmap(bmp, 50, 50, 0);
+		al_draw_triangle(20, 20, 300, 30, 200, 200, ALLEGRO_COLOR(1, 1, 1, 1), 4);
+		al_draw_text(font, ALLEGRO_COLOR(1, 1, 1, 1), 70, 40, ALLEGRO_ALIGN_CENTRE, "Hello!");
+		al_flip_display();
+		}
 	}
 
 void shutdown()
@@ -191,93 +468,12 @@ int main(char[][] args)
 	{
 	
 	return al_run_allegro(
-	{
-		if (!al_init())
-			{
-			auto ver = al_get_allegro_version();
-			auto major = ver >> 24;
-			auto minor = (ver >> 16) & 255;
-			auto revision = (ver >> 8) & 255;
-			auto release = ver & 255;
-
-			writefln("The system Allegro version (%s.%s.%s.%s) does not match the version of this binding (%s.%s.%s.%s)",
-				major, minor, revision, release,
-				ALLEGRO_VERSION, ALLEGRO_SUB_VERSION, ALLEGRO_WIP_VERSION, ALLEGRO_RELEASE_NUMBER);
-		
-			return 1;
-			}
-		
-		ALLEGRO_CONFIG* cfg = al_load_config_file("test.ini");
-		ALLEGRO_DISPLAY* display = al_create_display(500, 500);
-		ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-
-		if (!al_install_keyboard())      assert(0, "al_install_keyboard failed!");
-		if (!al_install_mouse())         assert(0, "al_install_mouse failed!");
-		if (!al_init_image_addon())      assert(0, "al_init_image_addon failed!");
-		if (!al_init_font_addon())       assert(0, "al_init_font_addon failed!");
-		if (!al_init_ttf_addon())        assert(0, "al_init_ttf_addon failed!");
-		if (!al_init_primitives_addon()) assert(0, "al_init_primitives_addon failed!");
-
-		al_register_event_source(queue, al_get_display_event_source(display));
-		al_register_event_source(queue, al_get_keyboard_event_source());
-		al_register_event_source(queue, al_get_mouse_event_source());
-
-		ALLEGRO_BITMAP* bmp = al_load_bitmap("./data/mysha.pcx");
-		ALLEGRO_FONT* font = al_load_font("./data/DejaVuSans.ttf", 18, 0);
-
-		with(ALLEGRO_BLEND_MODE)
-			{
-			al_set_blender(ALLEGRO_BLEND_OPERATIONS.ALLEGRO_ADD, ALLEGRO_ALPHA, ALLEGRO_INVERSE_ALPHA);
-			}
-
-		auto color1 = al_color_hsl(0, 0, 0);
-		auto color2 = al_map_rgba_f(0.5, 0.25, 0.125, 1);
-		writefln("%s, %s, %s, %s", color1.r, color1.g, color2.b, color2.a);
-		
-		bool exit = false;
-		while(!exit)
 		{
-			ALLEGRO_EVENT event;
-			while(al_get_next_event(queue, &event))
-			{
-				switch(event.type)
-				{
-					case ALLEGRO_EVENT_DISPLAY_CLOSE:
-					{
-						exit = true;
-						break;
-					}
-					case ALLEGRO_EVENT_KEY_DOWN:
-					{
-						switch(event.keyboard.keycode)
-						{
-							case ALLEGRO_KEY_ESCAPE:
-							{
-								exit = true;
-								break;
-							}
-							default:
-						}
-						break;
-					}
-					case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
-					{
-						exit = true;
-						break;
-					}
-					default:
-				}
-			}
-
-			al_clear_to_color(ALLEGRO_COLOR(0.5, 0.25, 0.125, 1));
-			al_draw_bitmap(bmp, 50, 50, 0);
-			al_draw_triangle(20, 20, 300, 30, 200, 200, ALLEGRO_COLOR(1, 1, 1, 1), 4);
-			al_draw_text(font, ALLEGRO_COLOR(1, 1, 1, 1), 70, 40, ALLEGRO_ALIGN_CENTRE, "Hello!");
-			al_flip_display();
-		}
-
+		initialize();
+		execute();
+		shutdown();
 		return 0;
-	});
+		} );
 
 
 	}
